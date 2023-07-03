8e17d07365a0a6344193b3bdc63256dbea088d85
Yuta Hayashibe <yuta@hayashibe.jp>
2023-07-03 10:18:38 +0900

    Added --sentences option to plain2request

--- pilota/convert/plain2request.py
+++ pilota/convert/plain2request.py
@@ -22,7 +22,18 @@ def get_context(items) -> list[Utterance]:
     return ret
 
 
-def operation(path_in: Path, path_out: Path) -> None:
+def operation(
+    *,
+    path_in: Path,
+    path_out: Path,
+    output_sentences: bool,
+) -> None:
+    bki = None
+    if output_sentences:
+        from bunkai import Bunkai
+
+        bki = Bunkai(path_model=None)
+
     with path_in.open() as inf, path_out.open("w") as outf:
         for items in csv.reader(inf, delimiter="\t"):
             uttr: str = ""
@@ -35,11 +46,20 @@ def operation(path_in: Path, path_out: Path) -> None:
                 ctx = get_context(items[:-1])
                 uttr = items[-1]
 
-            req = Request(
-                context=ctx,
-                utterance=uttr,
-                sentences=None,
-            )
+            req: Request
+            if output_sentences:
+                assert bki is not None
+                req = Request(
+                    context=ctx,
+                    utterance=None,
+                    sentences=[v for v in bki(uttr)],
+                )
+            else:
+                req = Request(
+                    context=ctx,
+                    utterance=uttr,
+                    sentences=None,
+                )
             outf.write(req.json(ensure_ascii=False))
             outf.write("\n")
 
@@ -48,12 +68,17 @@ def get_opts() -> argparse.Namespace:
     oparser = argparse.ArgumentParser()
     oparser.add_argument("--input", "-i", type=Path, default="/dev/stdin", required=False)
     oparser.add_argument("--output", "-o", type=Path, default="/dev/stdout", required=False)
+    oparser.add_argument("--sentences", action="store_true")
     return oparser.parse_args()
 
 
 def main() -> None:
     opts = get_opts()
-    operation(opts.input, opts.output)
+    operation(
+        path_in=opts.input,
+        path_out=opts.output,
+        output_sentences=opts.sentences,
+    )
 
 
 if __name__ == "__main__":
